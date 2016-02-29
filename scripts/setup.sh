a71529d8a1a687cbda43bed2712d1f2abb689915
German Rodriguez <german@phisoft.com.mx>
2016-02-29 14:36:29 -0700

    Proper setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,24 +1,54 @@
-echo "Setting up PostgreSQL database..."
-sudo -u postgres pg_dropcluster --stop $PG_VERSION main
-sudo -u postgres pg_createcluster --start $PG_VERSION main
-sudo -u postgres createuser -d -R -w -S vagrant
-perl -i -p -e 's/local   all             all                                     peer/local all all trust/' /etc/postgresql/9.3/main/pg_hba.conf
+#!/bin/bash
 
-echo "#################################################################################################"
-echo "# Welcome to the Dub5 Development Environment!                                                   "
-echo "#                                                                                                "
-echo "# Your are not done yet! You need to do the following to finish the setup:                       "
-echo "# 1) Get all the project repositories:                                                           "
-echo "#    Run: get-repositories                                                                       "
-echo "# 2) Get the porject dependencies:                                                               "
-echo "#    Run: cd /vagrant/dub5 ; bundle install                                                      "
-echo "# 3) Set up the application API keys. Put them in ~/.secret_keys.sh                              "
-echo "#                                                                                                "
-echo "# Info:                                                                                          "
-echo "# PostgreSQL user 'vagrant' created without a password. 'oodle_cashout_development' created.     "
-echo "#                                                                                                "
-echo "#################################################################################################"
-=======
+RUBY_VERSION=2.2.2
+RAILS_VERSION=4.2.2
+PG_VERSION=9.3
+
+execute_with_rbenv () {
+    `cat >/home/vagrant/temp-script.sh <<\EOF
+export HOME=/home/vagrant
+if [ -d $HOME/.rbenv ]; then
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+fi
+EOF
+`
+
+    echo $1 >> /home/vagrant/temp-script.sh
+    chmod +x /home/vagrant/temp-script.sh
+    su vagrant -c "bash -c /home/vagrant/temp-script.sh"
+    rm /home/vagrant/temp-script.sh
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get install libffi-dev libcurl4-gnutls-dev
+
+echo "dub5.dev" > /etc/hostname
+echo "127.0.0.1 dub5.dev" >> /etc/hosts
+
+`cat >/home/vagrant/.environment.sh <<\EOF
+# Environment variables
+export PS1="[\[\033[1;34m\]\u\[\033[0m\]@\h:\[\033[1;37m\]\w\[\033[0m\]]$ "
+alias l="ls -lAhG"
+alias ls="ls -G"
+alias ll="ls -lAhG"
+alias dir="ll"
+alias cls="clear"
+# Load secret keys, if any
+if [ -f ~/.secret_keys.sh ]; then
+  source ~/.secret_keys.sh
+fi
+EOF
+`
+
+echo 'source ~/.environment.sh' >> /home/vagrant/.bash_profile
+
+touch /home/vagrant/.secret_keys.sh
+
+chown vagrant:vagrant /home/vagrant/.environment.sh
+chown vagrant:vagrant /home/vagrant/.secret_keys.sh
+
 `cat >/home/vagrant/upgrade_rbenv.sh <<\EOF
 cd ~/.rbenv
 git pull
@@ -47,13 +77,9 @@ perl -i -p -e 's/local   all             all                                    
 `cat >/etc/nginx/sites-available/dub5-api <<\EOF
 server {
     server_name *.lvh.me;
-
     listen 3000;
-
     client_max_body_size 10M;
-
     passenger_enabled on;
-
     rails_env development;
     root /vagrant/dub5-api/public;
 }
